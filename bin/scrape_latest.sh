33df243c9ae3e200e54455b758e086bcd14fa67f
Glen Johnson <glenmarton@gmail.com>
2020-12-24 11:46:28 -0500

    Remove temp files generated by scrape_latest.sh

--- bin/scrape_latest.sh
+++ bin/scrape_latest.sh
@@ -7,4 +7,4 @@ sed -n 's/.*<a href=\"\([^"]*\)\".*/\1/p' ftplist.html \
 	| grep -- '[0-9]\{1,3\}\.[.0-9]\+\.tar.gz$' >release_list.txt
 
 VERSION=$(sed 's/.*-\([.0-9]\+\)\..*/\1/' release_list.txt | awk -f bin/newest.awk)
-grep "$VERSION" release_list.txt && rm -f release_list.txt ftplist.html
+grep "$VERSION" release_list.txt && rm -f ftplist.html release_list.txt >/dev/null 2>&1
