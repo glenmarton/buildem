bef94dcdfeac70598863d5a8703755d44dea9dfa
Glen Johnson <glenmarton@gmail.com>
2020-12-24 11:49:58 -0500

    Allow generation of perl.sh

--- bin/perl.sh
+++
@@ -1,9 +0,0 @@
-<pre>
-     wget <a href="https://www.cpan.org/src/5.0/perl-5.32.0.tar.gz">https://www.cpan.org/src/5.0/perl-5.32.0.tar.gz</a>
-     tar -xzf perl-5.32.0.tar.gz
-     cd perl-5.32.0
-     ./Configure -des -Dprefix=$HOME/localperl
-     make
-     make test
-     make install
-</pre>
