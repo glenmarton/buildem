f9ed7629ebafeb0c695f2407e83779933988feab
Glen Johnson <gjohnson@valcom.com>
2021-05-11 11:31:48 -0400

    Refactor build of libxz

--- bin/xzlib.sh
+++ bin/xzlib.sh
@@ -6,25 +6,55 @@ PREFIX=${PREFIX:=$HOME/.local}
 #
 #   f u n c t i o n s
 #
-checkout_latest_git_tag() {
-	TAG=$(git tag | awk -f $BIN/latest_tag.awk)
-	git checkout "$TAG" >/dev/null
+get_download_link_from() {
+	typeset url=$1
+	echo -n "$url"
+	curl $url 2>/dev/null | grep -m1 '<th.*href.*\.gz' | \
+		sed -n 's/.*files\/\(.*\)\/download\"/\1/p'
 }
+download_source() {
+	typeset url=$1
+	typeset SOURCE_TARBALL="${url##*/}"
 
+	if [ ! -f ~/Downloads/$SOURCE_TARBALL ]
+	then
+		wget $url
+		mv ${url##*/} ~/Downloads
+	fi
+	echo "$HOME/Downloads/$SOURCE_TARBALL"
+}
+unzip_file() {
+	typeset tarball=$1
+	typeset xzdir=${tarball%.tar*}
+	xzdir=${xzdir##*/}
+
+	if [ ! -d $xzdir ]
+	then
+		tar -xzf $tarball
+	fi
+
+	echo "$xzdir"
+}
 
 #
 #   s c r i p t
 #
-if ! test -d $TOP/xz*
+URL=$(get_download_link_from 'https://sourceforge.net/projects/lzmautils/files/')
+
+TARBALL=$(download_source $URL)
+
+DIR=$(unzip_file $TARBALL)
+cd $DIR
+
+if [ "$1" = '-t' ]
 then
-	typeset URL=$(curl https://sourceforge.net/projects/lzmautils/files/ | \
-		grep -m1 -A1 '<tr title="xz-.*\.gz' | sed -n 's/.*href="\(.*\)"$/\1/p')
-	curl -o ~/Downloads/xz.tar.gz $URL
-	tar -fxz ~/Downloads/xz.tar.gz
+	cat <<-EOF
+	URL=$URL
+	TARBALL=$TARBALL
+	DIR=$DIR
+	EOF
+else
+	./configure --prefix=$PREFIX
+	make all
+	make install || sudo make install
 fi
-
-cd $TOP/xz*
-
-./configure --prefix=$PREFIX
-make all
-make install || sudo make install
